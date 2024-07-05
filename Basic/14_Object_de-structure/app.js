aa594b52025f7789be6d8cb9c28024e5aa07cdab
syedzohaibalikazmi <syedzohaibali9900@gmail.com>
2024-07-05 21:41:34 +0500

    learn-Js

--- Basic/14_Object_de-structure/app.js
+++ Basic/14_Object_de-structure/app.js
@@ -1,5 +1,36 @@
 
-                            // De-Structure
+// ================================  De-Structure =====================
+
+const  course = {
+    course:"Web Development",
+    price: 1000,
+    teacher:"Zohaib"
+
+}
+   
+  const {teacher} =course
+
+//   console.log(teacher);
 
 
-                            
+// ================ Sir shehzad method ===================
+
+// for (const key in course) {
+//       console.log(key ,course[key]); // both key and value kay liya ya use karengay 
+//     }
+
+//     for (const key in course) {
+//         console.log(course[key]);  // value kauy liye use hota ha only
+//       }
+    
+// console.log(Object.entries(course))
+
+// ======================================= JSON ======================
+//  khali azume karen Json hota kasa ha ya sirf ek object ki tarhan dekhta ha 
+
+// {
+//     "name: "Zohaib ali",
+//     "course":"JavaScript",
+//     "fees": 5000
+
+// }
