222aa3431bcd7060dbd3ec4b7c614eb95266036e
syedzohaibalikazmi <syedzohaibali9900@gmail.com>
2024-08-04 02:37:02 +0500

    new

---
+++ Others/loops/app.js
@@ -0,0 +1,66 @@
+// console.log("zohaib ali");
+
+                    //  if else   
+
+
+// let age = 17
+
+// if (age>18) {
+//     console.log("you are enter in death zone");
+    
+// } else {
+//     console.log("you are child");
+    
+// }
+
+// ============================ loop=====================
+
+// let a = 0
+
+// for (let i = 0; i < 100; i++) {
+//  console.log(a+i);
+ 
+    
+// }
+
+// let obj ={
+//     name :"Zohaib ALi",
+//     Role: "Developer",
+//     company:"free"
+// }
+// for (const key in obj) {
+//      {
+//         const element = obj[key];
+//         console.log(key,element);
+        
+        
+//     }
+// }
+
+
+    
+// ========================= for off loop=================
+
+// for (const z of "object") {
+//     console.log( z)
+    
+// }
+
+// ========================= while loop=================
+
+// let b = 12;
+
+
+// while (b<20) {
+//     console.log(b);
+//     b++;
+    
+    
+// }
+
+// =========================Do while loop=================
+//  let c = 0
+
+//  do {
+//     console.log(c)
+//  } while (c<6);
