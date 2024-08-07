4197e926252b090b4a2f1e9054889593e9412d54
syedzohaibalikazmi <syedzohaibali9900@gmail.com>
2024-08-08 01:53:13 +0500

    Learn-Js

---
+++ Basic/17_Scope-Js/app.js
@@ -0,0 +1,23 @@
+
+// ========================================\ Block & Global Scope in Javascript/===============================
+
+// let a =400
+
+
+// //  bracket kay andar jo hota ha woh block scope hota ha or jo bahar hota ha woh global scope hota ha 
+// if (true) {
+    //     let a =10
+    //     console.log("Ineer value:", a)
+    //     // const b =50 
+    //     // var c=100
+    
+    // }
+    
+    // console.log(a);
+    // // console.log(b);
+// // console.log(c);
+
+    
+
+// ========================================\Scope level and mini hoisting in javascript /===============================
+
